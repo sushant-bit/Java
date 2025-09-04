fd550e0275770a88a2c269b70b41457212bccba5
sushant-bit <sushantadhikari944@gmail.com>
2025-09-04 18:00:31 +0545

    Add Java program to print inverted half pyramid star pattern
    - Implemented InvertedHalfPyramid.java to display an inverted half pyramid.
    - Used nested loops:
      - Outer loop starts from n down to 1.
      - Inner loop prints stars for each row.
    - Number of rows set to 5 by default (can be changed in the code).
    - Example output for n = 5:
      *****
      ****
      ***
      **
      *

---
+++ 9 pattern question in java/InvertedHalfPyramid.java
@@ -0,0 +1,15 @@
+public class InvertedHalfPyramid {
+    public static void main(String[] args) {
+        //inverted half pyramid;
+        int n = 5; //number of rows
+        //outer loop
+        for(int i=n;i>=1;i--){
+            //inner loop
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
