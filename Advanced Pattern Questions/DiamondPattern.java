005b8713d48a335755705a4f4c665850453e2dad
sushant-bit <sushantadhikari944@gmail.com>
2025-09-06 00:01:59 +0545

    Add Java program to print diamond pattern using nested loops
    Implemented DiamondPattern class with main method
    
    Prompt user to enter a number n
    
    Used nested loops to print the diamond pattern:
    
    Upper half with increasing stars and decreasing spaces
    
    Lower half with decreasing stars and increasing spaces
    
    Ensures symmetrical diamond shape output

---
+++ Advanced Pattern Questions/DiamondPattern.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class DiamondPattern {
+    public static void main(String[] args) {
+        System.out.println("Enter the number to print the Diamond pattern:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //outer loop
+        //upperhalf
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        // lowerhalf
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
