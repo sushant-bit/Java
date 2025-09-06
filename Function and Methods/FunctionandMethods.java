cee1a7122dc04adfae2b68c23c3ca450d20747e4
sushant-bit <sushantadhikari944@gmail.com>
2025-09-06 23:32:10 +0545

    Add examples of Java functions and methods (print name, sum, product, factorial)
    - Implemented multiple examples demonstrating the use of functions and methods in Java.
    - Added function `printYourName` to display a user's name.
    - Added function `calculateSum` to return the sum of two integers.
    - Added function `multiplyNumbers` to return the product of two integers.
    - Added function `printFactorial` to compute and print the factorial of a given number.
      - Includes input validation for negative numbers.
    - All examples take user input via Scanner and demonstrate function calls in the main method.
    
    These examples illustrate:
    1. Function declaration and definition syntax in Java.
    2. Using return types vs. void functions.
    3. Parameter passing and return values.
    4. Basic conditional checks and loop usage in functions.

--- Function and Methods/FunctionandMethods.java
+++ Function and Methods/FunctionandMethods.java
@@ -1,16 +1,77 @@
 /*returnType functionName(parameterType parameterName, ...) {
    operations//function body
    }*/
-import java.util.*;
-public class FunctionandMethods{
-   public static void printYourName(String name){
-      System.out.println("Your name is " + name);
-      return;
-   }
-   public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      String name =sc.next();
+// import java.util.*;
+// public class FunctionandMethods{
+//    public static void printYourName(String name){
+//       System.out.println("Your name is " + name);
+//       return;
+//    }
+//    public static void main(String[] args) {
+//       Scanner sc = new Scanner(System.in);
+//       String name =sc.next();
 
-      printYourName(name);//calling function
-   }
-} 
+//       printYourName(name);//calling function
+//    }
+// } 
+// import java.util.*;
+// public class FunctionandMethods{
+//   public static int calculateSum(int a, int b){
+//       int sum = a + b;
+//       return sum;
+//    }
+//    public static void main(String[] args) {
+//       System.out.println("Enter two numbers to find their sum");
+//       Scanner sc = new Scanner(System.in);
+//       int a=sc.nextInt();
+//       int b=sc.nextInt();
+//       int sum =calculateSum(a, b);
+
+//      System.out.println("The sum of two numbers is " + sum);
+
+      
+//    }
+// } 
+
+// import java.util.*;
+// public class FunctionandMethods{
+//   public static int multiplyNumbers(int a, int b){
+//       int product = a * b;
+//       return product;
+//    }
+//    public static void main(String[] args) {
+//       System.out.println("Enter two numbers to find their sum");
+//       Scanner sc = new Scanner(System.in);
+//       int a=sc.nextInt();
+//       int b=sc.nextInt();
+//       int product =multiplyNumbers(a, b);
+
+//      System.out.println("The sum of two numbers is " + product);
+
+      
+//    }
+// } 
+
+
+import java.util.*;
+
+public class FunctionandMethods {
+    public static void printFactorial(int a) {
+      if(a<0){
+         System.out.println("Factorial is not defined for negative numbers");
+         return;
+      }
+        int factorial = 1;
+        for (int i = a; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of " + a + " is: " + factorial);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to find its factorial:");
+        int a = sc.nextInt();
+        printFactorial(a);
+    }
+}
