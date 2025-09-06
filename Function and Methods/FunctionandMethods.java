24d42e42c0eedd162d8b1b29297082edf5091aa8
sushant-bit <sushantadhikari944@gmail.com>
2025-09-06 23:35:24 +0545

    Update FunctionandMethods.java

--- Function and Methods/FunctionandMethods.java
+++ Function and Methods/FunctionandMethods.java
@@ -53,25 +53,25 @@
 // } 
 
 
-import java.util.*;
+// import java.util.*;
 
-public class FunctionandMethods {
-    public static void printFactorial(int a) {
-      if(a<0){
-         System.out.println("Factorial is not defined for negative numbers");
-         return;
-      }
-        int factorial = 1;
-        for (int i = a; i >= 1; i--) {
-            factorial = factorial * i;
-        }
-        System.out.println("Factorial of " + a + " is: " + factorial);
-    }
+// public class FunctionandMethods {
+//     public static void printFactorial(int a) {
+//       if(a<0){
+//          System.out.println("Factorial is not defined for negative numbers");
+//          return;
+//       }
+//         int factorial = 1;
+//         for (int i = a; i >= 1; i--) {
+//             factorial = factorial * i;
+//         }
+//         System.out.println("Factorial of " + a + " is: " + factorial);
+//     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number to find its factorial:");
-        int a = sc.nextInt();
-        printFactorial(a);
-    }
-}
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number to find its factorial:");
+//         int a = sc.nextInt();
+//         printFactorial(a);
+//     }
+// }
