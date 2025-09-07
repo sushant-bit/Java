1e321760ed68bc6b925b2b660fd13507875f5bc8
sushant-bit <sushantadhikari944@gmail.com>
2025-09-07 22:04:37 +0545

    Update Sush.java

--- 1/Sush.java
+++ 1/Sush.java
@@ -3,5 +3,6 @@ class Sush{
         int number = 19;
         System.out.println("The number is: " + number*100);
         System.out.println("Sushant Adhikari");
+        System.out.println("I love to watch Movies");
     }
 }
