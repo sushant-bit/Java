20630299950db4e3915262413c7faac672cd4fac
sushant-bit <sushantadhikari944@gmail.com>
2025-09-14 22:25:49 +0545

    Add function to print sum of odd numbers up to n
    - Implemented PrintOddSum(int n) method to calculate and print the sum of all
      odd numbers from 1 to n.
    - Used a for loop with condition (i % 2 != 0) to identify odd numbers.
    - Added main method to take input from user via Scanner and call PrintOddSum.

--- Function and Methods/Exercise.java
+++ Function and Methods/Exercise.java
@@ -1,14 +1,32 @@
 import java.util.*;
 public class Exercise {
-   public static int CalculateAverage(int a, int b, int c){
-    int average = (a+b+c)/3;
-    return average;
-   }
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        System.out.println("Average of three number is :"+ CalculateAverage(a, b, c));
+//    public static int CalculateAverage(int a, int b, int c){
+//     int average = (a+b+c)/3;
+//     return average;
+//    }
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = sc.nextInt();
+//         System.out.println("Average of three number is :"+ CalculateAverage(a, b, c));
+//     }
+
+public static void PrintOddSum(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            sum=sum+i;
+        }
+
     }
+    System.out.println("Sum of odd numbers is :"+sum);
+
+}
+
+public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int n = sc.nextInt();
+    PrintOddSum(n);
+}
 }
