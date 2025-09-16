f1955c7ee2b92ee78cf8bdce547acafe36ce9f41
sushant-bit <sushantadhikari944@gmail.com>
2025-09-16 23:29:12 +0545

    Update TimeandSpaceComplexity.java

--- Basics of time and space complexity/TimeandSpaceComplexity.java
+++ Basics of time and space complexity/TimeandSpaceComplexity.java
@@ -1,5 +1,8 @@
 /*Basic of Time Complexity and Space Complexity
     Time Complexity
 =>Relation between input size and running time (operations)
-*Best case 
+*Best case -> Ω(1)
+*Average case -> Θ(n+1/2)
+*Worst case -> O(n)
+    Space Complexity
 */
