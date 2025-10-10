b58169a51ed08325ee763a08c2d9f3d3dd4717bc
sushant-bit <sushantadhikari944@gmail.com>
2025-10-10 12:51:13 +0545

    Starting Arrays

---
+++ Arrays/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+public static void main(String[] args) {
+    int[] marks = new int[3];
+    marks[0]=94;
+    marks[1]=98;
+    marks[2]=100;
+    // System.out.println(marks[0]);
+    //  System.out.println(marks[1]);
+    //   System.out.println(marks[2]);
+   
+    for(int i=0;i<3;i++){
+        System.out.println(marks[i]);
+    }
+}
+}
