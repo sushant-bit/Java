50f56cb3077b8b04215ada2c29f71a0e2604f624
sushant-bit <sushantadhikari944@gmail.com>
2025-12-07 14:46:45 +0545

    Update Arrays.java

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,15 +1,24 @@
+import java.util.*;
 public class Arrays {
 public static void main(String[] args) {
-    int[] marks = new int[3];
-    marks[0]=94;
-    marks[1]=98;
-    marks[2]=100;
+    //int[] marks = new int[3];
+    // int marks[]={94,98,100};
+    // marks[0]=94;
+    // marks[1]=98;
+    // marks[2]=100;
     // System.out.println(marks[0]);
     //  System.out.println(marks[1]);
     //   System.out.println(marks[2]);
    
-    for(int i=0;i<3;i++){
-        System.out.println(marks[i]);
+    // for(int i=0;i<3;i++){
+    //     System.out.println(marks[i]);
+    // }
+
+Scanner sc=new Scanner(System.in);
+int size=sc.nextInt();
+    int numbers[]= new int[size];
+    for(int i =0;i<size;i++){
+        System.out.println(numbers[i]);
     }
 }
 }
